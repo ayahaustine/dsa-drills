25264dfe72bbe9fcb5ebcfcba59d9361acba49b2
sphinx-austin <austinjuniour4@gmail.com>
2024-11-01 15:28:11 +0300

    added unsolved task

---
+++ python/unsolved/task3.py
@@ -0,0 +1,6 @@
+"""
+write a python function that takes a string 's'
+and a dict that contains (animal name, animal voice) 
+and returns the name of the animal whose voice is a 
+sub sequence of s. if multiple animal voices exist print any.
+"""
