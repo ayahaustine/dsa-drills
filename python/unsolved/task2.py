7aa1c3ad2f75c41f5b2df83d445da7bc6e9bfc7e
sphinx-austin <austinjuniour4@gmail.com>
2024-11-01 14:34:48 +0300

    new task

---
+++ python/unsolved/task2.py
@@ -0,0 +1,5 @@
+"""
+I need a function in Python that will take in an input word and create randomly
+generated acronyms out of it based on words found in an already-defined valid_words.
+I already defined valid_words, so you don't have to to define it, I just need the function.
+"""
