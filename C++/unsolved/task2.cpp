0ae6f4929140ab068faf7456afb22350b0e47d3a
sphinx-austin <austinjuniour4@gmail.com>
2024-11-03 22:47:38 +0300

    added new task

---
+++ C++/unsolved/task2.cpp
@@ -0,0 +1,7 @@
+/*
+Write a function in Python that will accept input of an array of numbers. 
+From this array, the function should create a 4x4 matrix, filling in the indices in spiral order.
+The first index filled should be the top left, then the first line should go right from there. 
+If the array has more elements than the matrix, replace the last index filled in the matrix with zero. 
+If the array doesn't have enough elements to fill the matrix, fill in the excess spots with 1s.
+*/
